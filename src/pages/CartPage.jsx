9df006391d5bbd3f5a5a18e7db290af0d844063c
Nicolas K <gabnicol0807@gmail.com>
2025-12-16 13:13:22 +0700

    Update react project

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,40 +1,24 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 export default function CartPage() {
     const { cart } = useContext(CartContext);
-    const totalPrice = cart.reduce((acc, item) => {
-        return acc + item.price;
-    }, 0);
-
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">Keranjang Belanjaan</h1>
-
-            {cart.length === 0 ? (
-                <div className="text-center mt-10">
-                    <p className="text-xl text-gray-500">Keranjang masih kosong...</p>
+        <div className="p-8 container mx-auto">
+            <div className="mt-8">
+                <h3 className="text-xl font-bold text-right">
+                    Total harga : Rp {totalPrice.toLocaleString("id-ID")}
+                </h3>
+                <div className="text-right mt-4">
+                    <Link to="/checkout">
+                        <button className="bg-blue-600 text-white px-6 py-2 rounded font-bold hover:bg-blue-700">
+                            Lanjut ke Pembayaran 👉
+                        </button>
+                    </Link>
                 </div>
-            ) : (
-                <div className="space-y-4">
-                    {cart.map((item, index) => (
-                        <div key={index} className="border p-4 shadow-sm rounded bg-white flex justify-between items-center">
-                            <div>
-                                <h3 className="text-xl font-semibold">{item.name}</h3>
-                                <p className="text-gray-600">Rp {item.price.toLocaleString("id-ID")}</p>
-                            </div>
-                            <button className="text-red-500 text-sm hover:underline">
-                                Hapus
-                            </button>
-                        </div>
-                    ))}
-                    <div className="mt-6 border-t pt-4 text-right">
-                        <h2 className="text-xl font-bold">
-                            Total harga : Rp {totalPrice.toLocaleString("id-ID")}
-                        </h2>
-                    </div>
-                </div>
-            )}
+            </div>
         </div>
     );
 }
