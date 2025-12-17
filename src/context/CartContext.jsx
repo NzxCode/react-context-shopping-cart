d5dddd15a23032d9a7a8db25f1b4671c37cf44a4
Nicolas K <gabnicol0807@gmail.com>
2025-12-17 12:45:01 +0700

    Update React

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext(null);
 
 export const CartProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState(() => {
+        const savedCart = localStorage.getItem("keranjang_nico");
+        return savedCart ? JSON.parse(savedCart) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("keranjang_nico", JSON.stringify(cart));
+    }, [cart]);
 
     const addToCart = (product) => {
         setCart([...cart, product]);
