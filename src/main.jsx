e68dd5d56e9c78a4690530ffc09cd6f2b9905c27
Nicolas K <gabnicol0807@gmail.com>
2025-12-20 12:40:17 +0700

    Update project react

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
     <CartProvider>
       <App />
     </CartProvider>
+    </AuthProvider>
   </BrowserRouter>
   </React.StrictMode>,
 )
