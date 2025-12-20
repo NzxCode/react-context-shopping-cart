89706add9d42598869c36b9a255f46607da6e1ef
Nicolas K <gabnicol0807@gmail.com>
2025-12-20 13:53:51 +0700

    Update React Project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,47 @@
 import { useContext } from 'react'
 import { CartContext } from '../context/CartContext';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from '../context/AuthContext.jsx';
 
 function Navbar() {
     const {cart} = useContext(CartContext);
+    const { currentUser, logout } = useAuth();
+    const navigate = useNavigate();
 
-    return (
-        <nav className="flex justify-between items-center bg-purple-600 p-4 text-white">
-            <Link to="/" className="text-2xl font-bold decoration-none text-white">
-                Toko Sembako Nico
-            </Link>
-            <Link to="/about" className="text-2xl font-semibold decoration-none text-white">
-                Tentang Toko Ini!
-            </Link>
-            <Link to="/cart" className="text-lg font-semibold decoration-none text-white">
-                Keranjang : {cart.length}
-            </Link>
-        </nav>
-    )
+async function handleLogout() {
+    try {
+        await logout();
+        navigate("/login");
+    } catch (error) {
+        console.log("Gagal logout:", error);
+    }
 }
 
+    return (
+        <nav className='flex justify-between items-center bg-purple-600 p-4 text-white sticky top-0 z-50 shadow-md'>
+            <Link to="/" className='text-2xl font-bold hover:text-purple-300'>Toko Sembako Nico</Link>
+             <div className="flex items-center gap-6">
+                {currentUser ? (
+                    <div className="flex items-center gap-4 border-r pr-4 border-purple-400">
+                        <span className="font-medium">Halo, {currentUser.email}
+                        </span>
+                        <button onClick={handleLogout} className="bg-red-500 px-3 py-1 rounded hover:bg-red-600 text-sm">
+                            Logout
+                        </button>
+                    </div>
+                ) : (
+                    <Link to="/login" className="bg-green-500 px-3 py-1 rounded hover:bg-green-600 text-sm">
+                        Login Masuk
+                    </Link>
+                )}
+                <Link to="/about" className='relative hover:text-purple-300'>
+                    Tentang Toko Kami
+                </Link>
+                <Link to="/cart" className='relative hover:text-purple-300'>
+                    Keranjang : {cart.length}
+                </Link>
+             </div>
+        </nav>
+    );
+}
 export default Navbar;
