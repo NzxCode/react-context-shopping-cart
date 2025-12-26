1f08274f391ab799dc9eeea231ada384beba23a6
Nicolas K <gabnicol0807@gmail.com>
2025-12-26 12:29:46 +0700

    Update Project E-Commerce React

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
-import { Routes, Route } from "react-router-dom";
 import DetailPage from "./pages/DetailPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import ProtectedRoute from "./components/ProtectedRoute";
 import SuccessPage from "./pages/SuccessPage";
 import OrderHistoryPage from "./pages/OrderHistoryPage";
-import AdminRoute from "./components/AdminRoute";
 import AdminDashboardPage from "./pages/AdminDashboardPage";
 import AddProductPage from "./pages/AddProductPage";
 import EditProductPage from "./pages/EditProductPage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -25,53 +25,44 @@ function App() {
         <Route path="/product/:id" element={<DetailPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/cart" element={
+          <ProtectedRoute>
+            <CartPage />
+          </ProtectedRoute>
+        } />
+        
+        <Route path="/checkout" element={
+          <ProtectedRoute>
+            <CheckoutPage />
+          </ProtectedRoute>
+        } />
+
         <Route path="/success" element={<SuccessPage />} />
-        <Route path="/history" element={
+        <Route path="/order-history" element={
           <ProtectedRoute>
             <OrderHistoryPage />
           </ProtectedRoute>
         } />
-        <Route
-          path="/cart"
-          element= {
-            <ProtectedRoute>
-              <CartPage />
-            </ProtectedRoute>
-          }
-        />
-          <Route 
-             path="/checkout"
-             element= {
-                <ProtectedRoute>
-                  <CheckoutPage />
-                </ProtectedRoute>
-             }
-        />
-          <Route
-            path="/admin"
-            element={
-              <AdminRoute>
-                <AdminDashboardPage />
-              </AdminRoute>
-            }
-          />
-          <Route path="/admin" element={
-            <AdminRoute>
-              <AdminDashboardPage />
-            </AdminRoute>
-          } />
-          <Route path="/admin/add-product" element={
-            <AdminRoute>
-              <AddProductPage />
-            </AdminRoute>
-          } />
-          <Route path="/admin/edit/:id" element={
-            <AdminRoute>
-              <EditProductPage />
-            </AdminRoute>
-          } />
+        <Route path="/admin" element={
+          <AdminRoute>
+            <AdminDashboardPage />
+          </AdminRoute>
+        } />
+        
+        <Route path="/admin/add-product" element={
+          <AdminRoute>
+            <AddProductPage />
+          </AdminRoute>
+        } />
+        
+        <Route path="/admin/edit/:id" element={
+          <AdminRoute>
+            <EditProductPage />
+          </AdminRoute>
+        } />
       </Routes>
     </div>
   );
 }
-export default App
+
+export default App;
