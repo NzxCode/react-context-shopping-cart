f93c9f55e89c1dd87296ec7247efdd99b21bad79
Nicolas K <gabnicol0807@gmail.com>
2025-12-26 10:56:52 +0700

    Update Project React

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAz22MdlfADCYJ-xVJtsP-FnTL_OYHVl30",
-  authDomain: "toko-nico-db.firebaseapp.com",
-  projectId: "toko-nico-db",
-  storageBucket: "toko-nico-db.firebasestorage.app",
-  messagingSenderId: "252960580934",
-  appId: "1:252960580934:web:1db0f5a30e1c0d117ff147"
+  apiKey: "AIzaSyAz7H_oFATefaU62_Sc1zUYk0j6ldUnvA0",
+  authDomain: "toko-nico-usa.firebaseapp.com",
+  projectId: "toko-nico-usa",
+  storageBucket: "toko-nico-usa.firebasestorage.app",
+  messagingSenderId: "799751290052",
+  appId: "1:799751290052:web:d4ddb6789b77c90f499d91"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
